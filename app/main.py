5b38e87a594413871400544f5a02f7e7c580323b
Adrianna Regulska <adrianna.regulska.1@gmail.com>
2025-03-21 13:05:28 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,4 @@
 import os
-import traceback
-from typing import Optional
 
 
 class CleanUpFile:
@@ -10,11 +8,7 @@ class CleanUpFile:
     def __enter__(self) -> "CleanUpFile":
         return self
 
-    def __exit__(self,
-                 exc_type: type,
-                 exc_value: Exception,
-                 traceback: Optional[traceback]) \
-            -> None:
+    def __exit__(self, *args) -> None:
         if os.path.exists(self.filename):
             os.remove(self.filename)
             print(f"{self.filename} has been deleted.")
